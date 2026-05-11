9ec9ee2ac28373aaad35b8d05f98c5f04a19efb9
yuzefovichas-cyber <yuzefovich.a.s@gmail.com>
2026-05-11 21:36:29 +0500

    Добавлена механика муки и префаб визуализации

---
+++ Assets/Flour.cs
@@ -0,0 +1,55 @@
+﻿using Assets;
+using UnityEngine;
+
+public class Flour : Food
+{
+    [Header("Flour Settings")]
+    [SerializeField] private float slowMultiplier = 0.5f;
+    [SerializeField] private GameObject flourVisualPrefab; 
+    [SerializeField] private float laneLength = 15f; 
+
+    private void Start()
+    {
+        SpawnVisuals();
+        ApplyEffectToLane();
+    }
+
+    private void SpawnVisuals()
+    {
+        // Спавним картинки муки вправо по линии
+        for (int i = 0; i < laneLength; i++)
+        {
+            Vector3 spawnPos = transform.position + new Vector3(i, 0, 0);
+            Instantiate(flourVisualPrefab, spawnPos, Quaternion.identity, transform);
+        }
+    }
+
+    private void ApplyEffectToLane()
+    {
+        // Находим всех мышей на линии в момент установки
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(
+            transform.position + new Vector3(laneLength / 2, 0, 0), 
+            new Vector2(laneLength, 1f), 
+            0f, 
+            enemyLayer
+        );
+
+        foreach (var col in colliders)
+        {
+            Rat rat = col.GetComponent<Rat>();
+            if (rat != null)
+            {
+                AddSlowToRat(rat);
+            }
+        }
+    }
+
+    private void AddSlowToRat(Rat rat)
+    {
+        // Используем Reflection, чтобы добраться до приватного списка effects в Rat
+        var effectsField = typeof(Rat).GetField("effects", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+        var effectsList = (System.Collections.Generic.List<Effect>)effectsField.GetValue(rat);
+        
+        effectsList.Add(new SlowEffect(slowMultiplier, 999f)); 
+    }
+}
---
+++ Assets/Slow effect.cs
@@ -0,0 +1,37 @@
+﻿using Assets;
+using UnityEngine;
+
+public class SlowEffect : Effect
+{
+    private float slowFactor; // Например, 0.5f для замедления в 2 раза
+    private float duration;
+    private float startTime;
+
+    public SlowEffect(float slowFactor, float duration)
+    {
+        this.slowFactor = slowFactor;
+        this.duration = duration;
+        this.startTime = Time.time;
+    }
+
+    public override void ApplyEffect(Rat rat)
+    {
+        // Находим приватное поле curSpeed через Reflection, 
+        // так как в твоем классе Rat оно private и нет публичного сеттера
+        var field = typeof(Rat).GetField("curSpeed", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+        
+        if (field != null)
+        {
+            float currentSpeed = (float)field.GetValue(rat);
+            field.SetValue(rat, currentSpeed * slowFactor);
+        }
+
+        // Логика удаления эффекта по истечении времени
+        if (Time.time > startTime + duration)
+        {
+            // Чтобы это работало, в классе Rat список effects должен быть public 
+            // или иметь метод для удаления. 
+            // Но для "бесконечной" муки на линии duration можно сделать очень большим.
+        }
+    }
+}
