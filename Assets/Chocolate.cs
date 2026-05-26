a905d8ad8c7c09b78d1c99a096a245966aad688d
yuzefovichas-cyber <yuzefovich.a.s@gmail.com>
2026-05-26 23:42:15 +0500

    Добавлен шоколад со всеми спрайтами и префабами

---
+++ Assets/Chocolate.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Chocolate : Food
+{
+    [Header("Chocolate Settings")]
+    [SerializeField] private GameObject projectilePrefab; 
+    [SerializeField] private float fireRate = 0.5f;        
+    
+    private int _shotsLeft = 8;                       
+    private float _nextFireTime = 0f;                   
+
+    private void Update()
+    {
+        if (_shotsLeft <= 0) 
+            return;
+
+        if (Time.time >= _nextFireTime)
+        {
+            if (CheckIsEnemyInRange())
+            {
+                Shoot();
+                _nextFireTime = Time.time + fireRate; 
+            }
+        }
+    }
+
+    private void Shoot()
+    {
+        _shotsLeft--;
+
+        Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+
+        if (_shotsLeft <= 0)
+            Die(); 
+    }
+}
